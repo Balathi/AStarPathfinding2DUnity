ea2c0b3e27eb30c44ef020b4827cce4943707388
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-08-06 22:44:50 +1200

    Add testing code

---
+++ COMP313Ass1/Assets/Code/Hello.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hello : MonoBehaviour {
+	public string myName;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am alive and my name is " + myName);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
