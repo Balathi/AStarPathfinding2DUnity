14b0b5da6767f22beb0c2ca6713456894844b645
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-08-06 22:39:31 +1200

    Add node class

---
+++ COMP313Ass1/Assets/Code/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Node {
+
+	public Vector3 GridPos;
+	public Vector3 Pos;
+	public bool isWall;
+
+	public Node Parent;
+
+	public int GCost;
+	public int HCost;
+
+	public Node(Vector3 gridPos, Vector3 pos, bool isWall) 
+	{
+		this.GridPos = gridPos;
+		this.Pos = pos;
+		this.isWall = isWall;
+	}
+
+	public int FCost() 
+	{
+		return GCost + HCost;
+	}
+
+}
