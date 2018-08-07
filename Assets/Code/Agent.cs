6dd23c8b6c64817b166b6e687796ce2ba5de58f4
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-08-07 18:34:05 +1200

    Made agents move towards target

--- Assets/Code/Agent.cs
+++ Assets/Code/Agent.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
+using NUnit.Framework.Internal.Execution;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Agent : MonoBehaviour {
-	
-	public List<Node> FinalPath;
+public class Agent : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public List<Vector3> Points;
+	private int destPoint = 0;
+	private bool start = true;
+	private int speed = 1;
+
+	private void GotoNextPoint()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, Points[destPoint], 0.02f);
+		destPoint = (destPoint + 1) % Points.Count;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void GotoPoint()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, Points[destPoint], 0.02f);
 	}
+
+	void Update()
+	{
+		if (Points.Count == 0) return;
+		if (Vector3.Distance(transform.position, Points[destPoint]) < 0.5f) GotoNextPoint();
+		GotoPoint();
+	}
+
 }
+
